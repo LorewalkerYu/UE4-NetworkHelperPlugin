d47c004f3632791a099dfd8ce34bb911af4b4eaa
LorewalkerYu <lorewalkeryu@foxmail.com>
2020-03-24 14:27:40 +0800

    RC Update

--- Source/RemoteControl/Private/Obj/RCInputObj.cpp
+++ Source/RemoteControl/Private/Obj/RCInputObj.cpp
@@ -7,41 +7,184 @@
 #include "Types/SlateStructs.h"
 #include "Framework/Application/SlateApplication.h"
 
-bool URCInputObj::SetMousePosition(int32 X, int32 Y)
-{
 #if PLATFORM_WINDOWS
-	// Client Area Rect
-	FSlateRect ClientRect = GEngine->GameViewport->GetWindow()->GetClientRectInScreen();
+#include "Windows/WindowsApplication.h"
+DECLARE_DELEGATE_ThreeParams(FMessageDelegate, uint32, WPARAM, LPARAM);
 
-	// x y coordinate convert
-	int cx_screen = ::GetSystemMetrics(SM_CXSCREEN);  //屏幕 宽
-	int cy_screen = ::GetSystemMetrics(SM_CYSCREEN);  //     高
+class FRCWindowsMessageHandler
+	: public IWindowsMessageHandler
+{
+public:
 
-	int real_x = 65535 * (X + int32(ClientRect.Left)) / cx_screen;  //转换后的 x
-	int real_y = 65535 * (Y + int32(ClientRect.Top)) / cy_screen;  //         y
-
-	INPUT InputMouse;
-	InputMouse.type = INPUT_MOUSE;
-	InputMouse.mi.mouseData = 0;
-	InputMouse.mi.dx = real_x;
-	InputMouse.mi.dy = real_y;
-	InputMouse.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
-	SendInput(1, &InputMouse, sizeof(INPUT));
-
-	return true;
-#endif
-	
-	// Be aware, this will not effect MouseX MouseY axis value
-	bool bRet = true;
-	if (!PlayerController)
+	// Just Listen, so always return false
+	virtual bool ProcessMessage(HWND hwnd, uint32 msg, WPARAM wParam, LPARAM lParam, int32& OutResult) override
 	{
+		MessageDelegate.ExecuteIfBound(msg, wParam, lParam);
 		return false;
 	}
 
-	PlayerController->SetMouseLocation(X, Y);
+public:
+
+	FMessageDelegate MessageDelegate;
+};
+FRCWindowsMessageHandler RCHandler;
+
+WPARAM MapLeftRightKeys(WPARAM vk, LPARAM lParam)
+{
+	WPARAM new_vk = vk;
+	UINT scancode = (lParam & 0x00ff0000) >> 16;
+	int extended = (lParam & 0x01000000) != 0;
+
+	switch (vk) {
+	case VK_SHIFT:
+		new_vk = MapVirtualKey(scancode, MAPVK_VSC_TO_VK_EX);
+		break;
+	case VK_CONTROL:
+		new_vk = extended ? VK_RCONTROL : VK_LCONTROL;
+		break;
+	case VK_MENU:
+		new_vk = extended ? VK_RMENU : VK_LMENU;
+		break;
+	default:
+		// not a key we map from generic to left/right specialized
+		//  just return it.
+		new_vk = vk;
+		break;
+	}
+
+	return new_vk;
+}
+#endif
+
+void URCInputObj::InitilizeRCForWindows(ERCType RemoteControlType, FMouseMove MouseMove, FKeyInput KeyInput)
+{
+	this->RCType = RemoteControlType;
+#if PLATFORM_WINDOWS
+	switch (RCType)
+	{
+	case ERCType::ERC_Receiver:
+	{
+
+	}break;
+	case ERCType::ERC_Sender:
+	{
+		
+		this->MouseMoveDele = MouseMove;
+		this->KeyInputDele = KeyInput;
+		RCHandler.MessageDelegate.BindLambda([this](uint32 msg, WPARAM wParam, LPARAM lParam)
+		{
+			switch (msg)
+			{
+			case WM_CHAR:
+			{
+				UE_LOG(LogTemp, Error, TEXT("Char"));
+			}break;
+			case WM_KEYDOWN:
+			{
+				const uint32 KeyCode = MapLeftRightKeys(wParam, lParam);
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Pressed);
+
+				if (!(lParam & 0x40000000))
+				{
+				}
+			}break;
+			case WM_KEYUP:
+			{
+				const uint32 KeyCode = MapLeftRightKeys(wParam, lParam);;
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Released);
+			}break;
+			case WM_LBUTTONDOWN:
+			{
+				const uint32 KeyCode = wParam;
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Pressed);
+				
+			}break;
+			case WM_LBUTTONUP:
+			{
+				const uint32 KeyCode = VK_LBUTTON;
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Released);
+			}break;
+			case WM_RBUTTONDOWN:
+			{
+				const uint32 KeyCode = wParam;
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Pressed);
+				
+			}break;
+			case WM_RBUTTONUP:
+			{
+				const uint32 KeyCode = VK_RBUTTON;
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Released);
+			}break;
+			case WM_MBUTTONDOWN:
+			{
+				const uint32 KeyCode = VK_MBUTTON;
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Pressed);
+				
+			}break;
+			case WM_MBUTTONUP:
+			{
+				const uint32 KeyCode = VK_MBUTTON;
+				const uint32 CharCode = MapVirtualKey(KeyCode, MAPVK_VK_TO_CHAR);
+				FKey Key = FInputKeyManager::Get().GetKeyFromCodes(KeyCode, CharCode);
+				this->KeyInputDele.ExecuteIfBound(Key.GetFName(), EInputEvent::IE_Released);
+			}break;
+			case WM_MOUSEMOVE:
+			{
+				this->MouseMoveDele.ExecuteIfBound(LOWORD(lParam), HIWORD(lParam));
+			}break;
+			case WM_MOUSEWHEEL:
+
+			default:
+				break;
+			}
+
+		});
+
+		FWindowsApplication* WindowsApp = (FWindowsApplication*)FSlateApplication::Get().GetPlatformApplication().Get();
+		if (WindowsApp)
+		{
+			WindowsApp->AddMessageHandler(RCHandler);
+		}
+
+	}break;
+	}
+#endif
+}
+
+bool URCInputObj::SetMousePosition(int32 X, int32 Y, int32 OtherViewportSizeX, int32 OtherViewportSizeY)
+{
+#if PLATFORM_WINDOWS
+	return SendInputMouseMove(X, Y, OtherViewportSizeX, OtherViewportSizeY);
+#endif
+	
 	return false;
 
 
+}
+
+bool URCInputObj::InputMouseWheel(bool bForward)
+{
+#if PLATFORM_WINDOWS
+	return SendInputMouseWheel(bForward);
+#endif
+	return false;
 }
 
 bool URCInputObj::InputKey(const FName KeyName, const EInputEvent InputEvent)
@@ -51,65 +194,9 @@ bool URCInputObj::InputKey(const FName KeyName, const EInputEvent InputEvent)
 	{
 		return false;
 	}
+
 #if PLATFORM_WINDOWS
-	INPUT Input;
-	int KeyCode = GetInputKeyCode(Key);
-	if(Key.IsMouseButton())
-	{
-		Input.type = INPUT_MOUSE;
-
-		// Left Mouse Button
-		if (KeyCode == VK_LBUTTON)
-		{
-			if (InputEvent == EInputEvent::IE_Pressed)
-			{
-				Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
-			}
-			if (InputEvent == EInputEvent::IE_Released)
-			{
-				Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
-			}
-		}
-		
-		// Right Mouse Button
-		if (KeyCode == VK_RBUTTON)
-		{
-			if (InputEvent == EInputEvent::IE_Pressed)
-			{
-				Input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
-			}
-			if (InputEvent == EInputEvent::IE_Released)
-			{
-				Input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
-			}
-		}
-
-		// Middle Mouse Button
-		if (KeyCode == VK_MBUTTON)
-		{
-			if (InputEvent == EInputEvent::IE_Pressed)
-			{
-				Input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
-			}
-			if (InputEvent == EInputEvent::IE_Released)
-			{
-				Input.mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
-			}
-		}
-	}
-	else
-	{
-		Input.type = INPUT_KEYBOARD;
-		Input.ki.wVk = GetInputKeyCode(Key);
-		if (InputEvent == EInputEvent::IE_Released)
-		{
-			Input.ki.dwFlags = KEYEVENTF_KEYUP;
-		}
-	}
-	
-	
-	SendInput(1, &Input, sizeof(INPUT));
-	return true;
+	return SendInputKey(Key, InputEvent);
 #endif
 
 	// Some problem to be fixed;
@@ -138,11 +225,106 @@ bool URCInputObj::InputKey(const FName KeyName, const EInputEvent InputEvent)
 	}
 }
 
-bool URCInputObj::InputKeyTest(const FName KeyName, const EInputEvent InputEvent)
+int URCInputObj::GetInputKeyCode(const FKey& Key)
 {
-	bool bRet = false;
+	FInputKeyManager Manager = FInputKeyManager::Get();
 
-	return bRet;
+	const uint32* KeyCode;
+	const uint32* CharCode;
+	FString KeyName = Key.GetFName().ToString();
+
+	if (KeyName.Len() == 1)
+	{
+		return KeyName.GetCharArray()[0];
+	}
+
+	Manager.GetCodesFromKey(Key, KeyCode, CharCode);
+
+	if (KeyCode)
+	{
+		return *KeyCode;
+	}
+
+	if (CharCode)
+	{
+		return *CharCode;
+	}
+
+	if (KeyName.Contains("Num"))
+	{
+		FString NumPad = KeyName.Replace(TEXT("Num"), TEXT(""));
+		if (NumPad.IsNumeric())
+		{
+			return FCString::Atoi(*NumPad) + 0x60;
+		}
+		if (NumPad == "/")
+		{
+			return VK_DIVIDE;
+		}
+		if (NumPad == "*")
+		{
+			return VK_MULTIPLY;
+		}
+		if (NumPad == "-")
+		{
+			return VK_SUBTRACT;
+		}
+		if (NumPad == "+")
+		{
+			return VK_ADD;
+		}
+		if (NumPad == ".")
+		{
+			return VK_DECIMAL;
+		}
+	}
+
+	return 0;
+}
+
+TCHAR URCInputObj::GetInputCharacter(const FKey& Key)
+{
+	FInputKeyManager&  InputKeyManager = FInputKeyManager::Get();
+	FSlateApplication& SlateApp = FSlateApplication::Get();
+
+	const uint32* KeyCode = nullptr;
+	const uint32* CharCode = nullptr;
+
+	InputKeyManager.GetCodesFromKey(Key, KeyCode, CharCode);
+
+	if (CharCode)
+	{
+		// A ~ Z
+		if (*CharCode >= 65 && *CharCode <= 106)
+		{
+			return (SlateApp.GetPlatformApplication()->GetModifierKeys().AreCapsLocked() || SlateApp.GetPlatformApplication()->GetModifierKeys().IsLeftShiftDown() || SlateApp.GetPlatformApplication()->GetModifierKeys().IsRightShiftDown()) ? (*CharCode + 32) : (*CharCode);
+		}
+
+		if (SlateApp.GetPlatformApplication()->GetModifierKeys().IsLeftShiftDown() || SlateApp.GetPlatformApplication()->GetModifierKeys().IsRightShiftDown())
+		{
+			if (*CharCode >= 48 && *CharCode <= 57)
+			{
+
+
+			}
+		}
+		// 0 ~ 9 
+		
+
+		return *CharCode;
+	}
+
+	FString KeyName = Key.GetFName().ToString();
+	if (KeyName.Contains("Num"))
+	{
+		FString NumPad = KeyName.Replace(TEXT("Num"), TEXT(""));
+		if (NumPad.IsNumeric())
+		{
+			return FCString::Atoi(*NumPad);
+		}
+		return NumPad[0];
+	}
+	return NULL;
 }
 
 bool URCInputObj::InputChar(const FKey& Key, const EInputEvent& InputEvent)
@@ -249,104 +431,115 @@ bool URCInputObj::InputMouseButtonUI(const FKey& Key, const EInputEvent& InputEv
 	return false;
 }
 
-int URCInputObj::GetInputKeyCode(const FKey& Key)
+bool URCInputObj::SendInputMouseMove(int32 X, int32 Y, int32 OtherViewportSizeX, int32 OtherViewportSizeY)
 {
-	FInputKeyManager Manager = FInputKeyManager::Get();
+	// Client Area Rect
+	FSlateRect ClientRect = GEngine->GameViewport->GetWindow()->GetClientRectInScreen();
 
-	const uint32* KeyCode;
-	const uint32* CharCode;
-	FString KeyName = Key.GetFName().ToString();
+	// x y coordinate convert
+	
+	double TargetX   = double(X) / double(OtherViewportSizeX) * (ClientRect.Right  - ClientRect.Left);
+	double TargetY   = double(Y) / double(OtherViewportSizeY) * (ClientRect.Bottom - ClientRect.Top );
 
-	if (KeyName.Len() == 1)
+	int ScreenWidth  = ::GetSystemMetrics(SM_CXSCREEN);
+	int ScreenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+	long real_x      = (TargetX + ClientRect.Left) * double(65535 / ScreenWidth);
+	long real_y      = (TargetY + ClientRect.Top ) * double(65535 / ScreenHeight);
+
+	INPUT InputMouse;
+	InputMouse.type = INPUT_MOUSE;
+	InputMouse.mi.mouseData = 0;
+	InputMouse.mi.dx = real_x;
+	InputMouse.mi.dy = real_y;
+	InputMouse.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+
+	UE_LOG(LogTemp, Error, TEXT("%d, %d  ---   %d, %d"), X, Y, real_x, real_y);
+	if (GEngine)
 	{
-		return KeyName.GetCharArray()[0];
+		FString str = FString::FromInt(X) + "," + FString::FromInt(Y) + "   -----   " + FString::FromInt(real_x) + "," + FString::FromInt(real_y);
+		GEngine->AddOnScreenDebugMessage(0, 0.1f, FColor::Blue, *str);
 	}
-
-	Manager.GetCodesFromKey(Key, KeyCode, CharCode);
-
-	if (KeyCode)
-	{
-		return *KeyCode;
-	}
-
-	if (CharCode)
-	{
-		return *CharCode;
-	}
-
-	if (KeyName.Contains("Num"))
-	{
-		FString NumPad = KeyName.Replace(TEXT("Num"), TEXT(""));
-		if (NumPad.IsNumeric())
-		{
-			return FCString::Atoi(*NumPad) + 0x60;
-		}
-		if (NumPad == "/")
-		{
-			return VK_DIVIDE;
-		}
-		if (NumPad == "*")
-		{
-			return VK_MULTIPLY;
-		}
-		if (NumPad == "-")
-		{
-			return VK_SUBTRACT;
-		}
-		if (NumPad == "+")
-		{
-			return VK_ADD;
-		}
-		if (NumPad == ".")
-		{
-			return VK_DECIMAL;
-		}
-	}
-
-	return 0;
+	return ::SendInput(1, &InputMouse, sizeof(INPUT));
 }
 
-TCHAR URCInputObj::GetInputCharacter(const FKey& Key)
+bool URCInputObj::SendInputKey(const FKey& Key, const EInputEvent& InputEvent)
 {
-	FInputKeyManager&  InputKeyManager = FInputKeyManager::Get();
-	FSlateApplication& SlateApp        = FSlateApplication::Get();
+	INPUT Input;
+	int KeyCode = GetInputKeyCode(Key);
 
-	const uint32* KeyCode  = nullptr;
-	const uint32* CharCode = nullptr;
-
-	InputKeyManager.GetCodesFromKey(Key, KeyCode, CharCode);
-	
-	if (CharCode)
+	if (Key.IsMouseButton())
 	{
-		// A ~ Z
-		if (*CharCode >= 65 && *CharCode <= 106)
-		{
-			return (SlateApp.GetPlatformApplication()->GetModifierKeys().AreCapsLocked() || SlateApp.GetPlatformApplication()->GetModifierKeys().IsLeftShiftDown() || SlateApp.GetPlatformApplication()->GetModifierKeys().IsRightShiftDown()) ? (*CharCode + 32) : (*CharCode);
-		}
-		
-		if (SlateApp.GetPlatformApplication()->GetModifierKeys().IsLeftShiftDown() || SlateApp.GetPlatformApplication()->GetModifierKeys().IsRightShiftDown())
-		{
-			if (*CharCode >= 48 && *CharCode <= 57)
-			{
-				
+		Input.type = INPUT_MOUSE;
 
+		// Left Mouse Button
+		if (KeyCode == VK_LBUTTON)
+		{
+			if (InputEvent == EInputEvent::IE_Pressed)
+			{
+				Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+			}
+			if (InputEvent == EInputEvent::IE_Released)
+			{
+				Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
 			}
 		}
-		// 0 ~ 9 
-		
-		
-		return *CharCode;
+
+		// Right Mouse Button
+		if (KeyCode == VK_RBUTTON)
+		{
+			if (InputEvent == EInputEvent::IE_Pressed)
+			{
+				Input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+			}
+			if (InputEvent == EInputEvent::IE_Released)
+			{
+				Input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+			}
+		}
+
+		// Middle Mouse Button
+		if (KeyCode == VK_MBUTTON)
+		{
+			if (InputEvent == EInputEvent::IE_Pressed)
+			{
+				Input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+			}
+			if (InputEvent == EInputEvent::IE_Released)
+			{
+				Input.mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+			}
+		}
+	}
+	else
+	{
+		Input.type = INPUT_KEYBOARD;
+		Input.ki.wVk = KeyCode;
+		Input.ki.wScan = MapVirtualKey(KeyCode, MAPVK_VK_TO_VSC);
+		Input.ki.dwFlags = 0;
+		if (InputEvent == EInputEvent::IE_Released)
+		{
+			Input.ki.dwFlags = KEYEVENTF_KEYUP;
+		}
 	}
 
-	FString KeyName = Key.GetFName().ToString();
-	if (KeyName.Contains("Num"))
-	{
-		FString NumPad = KeyName.Replace(TEXT("Num"), TEXT(""));
-		if (NumPad.IsNumeric())
-		{
-			return FCString::Atoi(*NumPad);
-		}
-		return NumPad[0];
-	}
-	return NULL;
+	
+	return ::SendInput(1, &Input, sizeof(INPUT));;
 }
+
+bool URCInputObj::SendInputMouseWheel(bool bForward)
+{
+	INPUT Input;
+	Input.type = INPUT_MOUSE;
+	Input.mi.dwFlags = MOUSEEVENTF_WHEEL;
+	if (bForward)
+	{
+		Input.mi.mouseData = WHEEL_DELTA;
+	}
+	else
+	{
+		Input.mi.mouseData = WHEEL_DELTA * -1;
+	}
+	
+	return ::SendInput(1, &Input, sizeof(INPUT));
+}
+
--- Source/RemoteControl/Public/Obj/RCInputObj.h
+++ Source/RemoteControl/Public/Obj/RCInputObj.h
@@ -5,41 +5,66 @@
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "GameFramework/PlayerController.h"
+
 #include "RCInputObj.generated.h"
 
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FMouseMove, int32, X, int32, Y);
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FKeyInput, const FName, KeyName, const EInputEvent, InputEvent);
 /**
  * 
  */
+UENUM(BlueprintType)
+enum class ERCType : uint8
+{
+	ERC_None     UMETA(DisplayName = "None"),
+	ERC_Sender   UMETA(DisplayName = "Sender"),
+	ERC_Receiver UMETA(DisplayName = "Receiver"),
+};
 
-UCLASS()
+
+UCLASS(NotBlueprintType)
 class REMOTECONTROL_API URCInputObj : public UObject
 {
 	GENERATED_BODY()
 
 public:
 
+	UFUNCTION(BlueprintCallable, Category = "NetworkHelperPlugin|RemoteControl|Input")
+		void InitilizeRCForWindows(ERCType RemoteControlType, UPARAM(DisplayName = "MouseMoveEvent") FMouseMove MouseMove, UPARAM(DisplayName = "KeyInputEvent") FKeyInput KeyInput);
+
 	/** Positions the mouse cursor in screen space, in pixels. */
-	UFUNCTION(BlueprintCallable, Category = "NetworkHelperPlugin|RemoteControl|Input")
-		bool SetMousePosition(int32 X, int32 Y);
+	UFUNCTION(BlueprintCallable, meta = (BlueprintProtected = "true"), Category = "NetworkHelperPlugin|RemoteControl|Input")
+		bool SetMousePosition(int32 X, int32 Y, int32 OtherViewportSizeX, int32 OtherViewportSizeY);
 
-	UFUNCTION(BlueprintCallable, Category = "NetworkHelperPlugin|RemoteControl|Input")
+	UFUNCTION(BlueprintCallable, meta = (BlueprintProtected = "true"), Category = "NetworkHelperPlugin|RemoteControl|Input")
+		bool InputMouseWheel(bool bForward);
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintProtected = "true"), Category = "NetworkHelperPlugin|RemoteControl|Input")
 		bool InputKey(const FName KeyName, const EInputEvent InputEvent);
-
-	UFUNCTION(BlueprintCallable, Category = "NetworkHelperPlugin|RemoteControl|Input")
-		bool InputKeyTest(const FName KeyName, const EInputEvent InputEvent);
 
 protected:
 
-	bool  InputChar(const FKey& Key, const EInputEvent& InputEvent);
-	bool  InputKeyGamePlay(const FKey& Key, const EInputEvent& InputEvent);
-	bool  InputKeyUI(const FKey& Key, const EInputEvent& InputEvent);
-	bool  InputMouseButtonUI(const FKey& Key, const EInputEvent& InputEvent);
-	int   GetInputKeyCode(const FKey& Key);
-	TCHAR GetInputCharacter(const FKey& Key);
+	int   GetInputKeyCode     (const FKey& Key);
+	TCHAR GetInputCharacter   (const FKey& Key);
+
+
+	bool  InputChar           (const FKey& Key, const EInputEvent& InputEvent);
+	bool  InputKeyGamePlay    (const FKey& Key, const EInputEvent& InputEvent);
+	bool  InputKeyUI          (const FKey& Key, const EInputEvent& InputEvent);
+	bool  InputMouseButtonUI  (const FKey& Key, const EInputEvent& InputEvent);
+
+	bool  SendInputMouseMove  (int32 X, int32 Y, int32 OtherViewportSizeX, int32 OtherViewportSizeY);
+	bool  SendInputKey        (const FKey& Key, const EInputEvent& InputEvent);
+	bool  SendInputMouseWheel (bool bForward);
+
 
 public:
 
 	UPROPERTY(BlueprintReadOnly, meta = (ExposeOnSpawn = true))
 		APlayerController* PlayerController = nullptr;
 
+	ERCType RCType;
+
+	FMouseMove MouseMoveDele;
+	FKeyInput KeyInputDele;
 };
